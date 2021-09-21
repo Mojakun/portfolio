24f3d4357616f97317338c75ea04092993d88fe4
Mojakun <kazuya.6289@gmail.com>
2021-09-22 08:43:24 +0900

    環境構築終わりかな

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import type { NextPage } from 'next';
-
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import WAvatar from '@/components/atoms/avatar/WAvatar';
 import MainLayout from '@/components/templates/MainLayout';
 
 const Home: NextPage = () => {
